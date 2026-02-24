2f57bf8f51d047c7720e8a6f7e19333c7b359224
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-02-24 14:43:49 +0100

    Make studio overlay opt-in for sample startup

--- samples/SourceGenCrudSample/Program.cs
+++ samples/SourceGenCrudSample/Program.cs
@@ -15,20 +15,33 @@ internal static class Program
 
     public static AppBuilder BuildAvaloniaApp()
     {
-        return AppBuilder.Configure<App>()
+        var builder = AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .LogToTrace()
             .UseAvaloniaSourceGeneratedXaml()
-            .UseAvaloniaSourceGeneratedStudio(options =>
-            {
-                options.PersistChangesToSource = true;
-                options.WaitMode = SourceGenStudioWaitMode.WaitForLocalOnly;
-                options.UpdateTimeout = TimeSpan.FromSeconds(10);
-                options.FallbackPolicy = SourceGenStudioFallbackPolicy.RuntimeApplyOnTimeout;
-                options.ShowOverlayIndicator = true;
-                options.EnableExternalWindow = true;
-                options.AutoOpenStudioWindowOnStartup = true;
-            })
             .UseAvaloniaSourceGeneratedXamlIdeHotReloadFallback(enable: true, pollingIntervalMs: 1000);
+
+        if (!IsStudioEnabled())
+        {
+            return builder;
+        }
+
+        return builder.UseAvaloniaSourceGeneratedStudio(options =>
+        {
+            options.PersistChangesToSource = true;
+            options.WaitMode = SourceGenStudioWaitMode.WaitForLocalOnly;
+            options.UpdateTimeout = TimeSpan.FromSeconds(10);
+            options.FallbackPolicy = SourceGenStudioFallbackPolicy.RuntimeApplyOnTimeout;
+            options.ShowOverlayIndicator = true;
+            options.EnableExternalWindow = true;
+            options.AutoOpenStudioWindowOnStartup = false;
+        });
+    }
+
+    private static bool IsStudioEnabled()
+    {
+        var value = Environment.GetEnvironmentVariable("AXSG_STUDIO");
+        return string.Equals(value, "1", StringComparison.OrdinalIgnoreCase) ||
+               string.Equals(value, "true", StringComparison.OrdinalIgnoreCase);
     }
 }
