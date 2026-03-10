a9ae323da5ed11d49210f1372ac47fdf80aff01b
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-03-10 16:05:40 +0100

    docs: clean generated API surface

--- src/XamlToCSharpGenerator.Runtime.Core/IgnoresAccessChecksToAttribute.cs
+++ src/XamlToCSharpGenerator.Runtime.Core/IgnoresAccessChecksToAttribute.cs
@@ -1,9 +1,13 @@
+// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
 using System;
 
 namespace System.Runtime.CompilerServices;
 
+/// <exclude />
 [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
-public sealed class IgnoresAccessChecksToAttribute : Attribute
+internal sealed class IgnoresAccessChecksToAttribute : Attribute
 {
     public IgnoresAccessChecksToAttribute(string assemblyName)
     {
