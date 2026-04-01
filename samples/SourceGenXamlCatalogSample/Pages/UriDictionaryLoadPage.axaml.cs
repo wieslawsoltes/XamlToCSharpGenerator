a9f9d51bdf0462d06e80c0cee3e39ac8e3bcb27a
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-04-01 21:13:02 +0200

    Add catalog sample for URI-loaded resource dictionaries

---
+++ samples/SourceGenXamlCatalogSample/Pages/UriDictionaryLoadPage.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SourceGenXamlCatalogSample.Pages;
+
+public partial class UriDictionaryLoadPage : UserControl
+{
+    public UriDictionaryLoadPage()
+    {
+        InitializeComponent();
+    }
+}
